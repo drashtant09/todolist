8b46c9a9d35b1b0cd18073330df2e84d3ecb737b
drashtant09 <gangwar.drashtant@gmail.com>
2023-04-29 15:10:36 +0530

    3rd commit

--- src/components/todoreact/Todo.jsx
+++ src/components/todoreact/Todo.jsx
@@ -1,6 +1,16 @@
-import React from 'react'
+import React, { useState } from 'react'
 import "./style.css";
 export const Todo = () => {
+  const [inputdata,setInputData] = useState("")
+  const [items, setItems] = useState([])
+  // add the items function
+  const addItems = () => {
+    if(!inputdata) {
+      alert("plz fill the data")
+    }else {
+      setItems([...items, inputdata])
+    }
+  }
   return (
     <div>
       <div className="main-div">
@@ -15,7 +25,31 @@ export const Todo = () => {
         type="text"
         placeholder="✍ Add Item"
         className="form-control"
+        value={inputdata}
+        onChange={(event) => setInputData(event.target.value)}
         />
+        <i className="fa fa-plus add-btn" onClick={addItems}></i>
+       </div>
+       {/*show our items*/}
+       <div className="showItems">
+        {items.map((curElem, index) => {
+        return (
+          <div className="eachItem" key={index}>
+          <h3>{curElem}</h3>
+          <div className="todo-btn">
+            <i className="far fa-edit add-btn"></i>
+            <i className="far fa-trash-alt add-btn"></i>
+          </div>
+        </div>
+        )
+        })}
+      
+       </div>
+       {/*rmove all button*/}
+       <div className="showItems">
+        <button className="btn effect04" data-sm-link-text="Remove All">
+          <span>CHECK LIST</span>
+        </button>
        </div>
     </div>
 </div>
